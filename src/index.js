3c629e0815706b0ddf8563786edf1fc418880d83
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2024-07-16 15:10:56 +0100

    First commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './style.css';
+
+if (process.env.NODE_ENV !== 'production') {
+	console.log('DEV MODE');
+} else if (process.env.NODE_ENV !== 'develompent') {
+	console.log('PRODUCTION MODE');
+}
