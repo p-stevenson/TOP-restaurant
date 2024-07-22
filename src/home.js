a5f108d38cce2281bcafadf3d311fec887f1512b
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2024-07-22 14:48:20 +0100

    Add eleCreate utility function to home.js

--- src/home.js
+++ src/home.js
@@ -1,33 +1,40 @@
 import frontEntrance from './images/tfrFront.jpg'
 
+const eleCreate = function (type, attributes = {}, parentElement) {
+	const element = document.createElement(type);
+	for (const attribute in attributes) {
+		if (attribute === 'textContent') {
+			element.textContent = attributes[attribute];
+		} else if (attribute === 'src') {
+			element.src = attributes[attribute];
+		} else {
+			element.setAttribute(attribute, attributes[attribute]);
+		}
+	}
+	if (parentElement) {
+		parentElement.appendChild(element);
+	}
+	return element;
+}
+
 export function displayContent() {
 	const mainContainer = document.getElementById('divcontent');
-	const headingContainer = document.createElement('div');
-	headingContainer.id = 'headingContainer';
-	mainContainer.appendChild(headingContainer);
 
-	const mainHeading = document.createElement('h1');
-	mainHeading.textContent = ('The Taste Tester');
-	headingContainer.appendChild(mainHeading)
+	const headingContainer = eleCreate('div', {id: 'headingContainer'}, mainContainer)
+	eleCreate('h1', {id: 'mainHeading', textContent: 'The Taste Tester'}, headingContainer)
 
-	const heroContainer = document.createElement('div');
-	heroContainer.id = 'heroContainer';
-	mainContainer.appendChild(heroContainer);
+	const heroContainer = eleCreate('div', {id: 'heroContainer'}, mainContainer)
+	eleCreate('img', {
+		id: 'heroImg',
+		alt: 'The Grand front entrance of the restaruant',
+		height: 500,
+		src: frontEntrance
+	}, heroContainer);
 
-	const heroImg = document.createElement('img');
-	heroImg.src = frontEntrance;
-	heroContainer.appendChild(heroImg);
-
-	const taglineContainer = document.createElement('div');
-	taglineContainer.id = 'taglineContainer';
-	mainContainer.appendChild(taglineContainer);
-
-	const emphasize = document.createElement("em");
-
-	taglineContainer.appendChild(emphasize);
-
-	const tagline = document.createElement('p');
-	tagline.id = 'tagline';
-	tagline.textContent = 'A fine dining experience for the most obnoxiously refined of diners.';
-	emphasize.appendChild(tagline);
+	const taglineContainer = eleCreate('div', {id: 'taglineContainer'}, mainContainer);
+	const emphasize = eleCreate('em', {}, taglineContainer);
+	eleCreate('p', {
+		id: 'tagline',
+		textContent: 'A fine dining experience for the most obnoxiously refined of diners.'
+	}, emphasize)
 }
